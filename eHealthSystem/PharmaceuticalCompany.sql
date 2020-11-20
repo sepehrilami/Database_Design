c848d4cfb2c1125c1210a9db0210dea1224db4d9
ahmadsalimi <ahsa9978@gmail.com>
2020-11-20 16:00:13 +0330

    merge

--- eHealthSystem/PharmaceuticalCompany.sql
+++
@@ -1,9 +0,0 @@
--- No Dependency
-
-CREATE TABLE `PharmaceuticalCompany` (
-    `Name` varchar(30) NOT NULL,
-    `Phone` varchar(15) NOT NULL,
-    PRIMARY KEY (`Name`)
-);
-
-INSERT INTO `PharmaceuticalCompany`(`Name`, `Phone`) VALUES('Company1', '0216616');
--- eShop/Cart.sql
+++
@@ -1,9 +0,0 @@
--- Depends on User
-
-CREATE TABLE `Cart` (
-    `UserEmail` varchar(100) NOT NULL,
-    PRIMARY KEY (`UserEmail`),
-    CONSTRAINT `FK_Cart_User_UserEmail` FOREIGN KEY (`UserEmail`) REFERENCES `User` (`Email`) ON DELETE CASCADE
-);
-
-INSERT INTO `Cart`(`UserEmail`) VALUES('sepehr@ilami.com');
